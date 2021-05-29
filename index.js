1c09cc03ec48bff9a3dec35c0eb10fa576572e96
ethantaylan <ethtaylan@gmail.com>
2021-05-29 23:08:23 +0200

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var navProduits = document.getElementById ("nav-produits")
 var navPanier = document.getElementById ("navPanier")
 var navContact = document.getElementById ("nav-contact")
 var close = document.getElementById("close")
-
+var boite = document.querySelector(".box-teddy")
 var pageWelcome = document.getElementById("page-welcome")
 var pageAccueil = document.getElementById("page-accueil")
 var pageProduits = document.getElementById("page-produits")
@@ -19,4 +19,7 @@ navEnter.onclick = function() {
 
 navProduits.onclick = function() {
     pageProduits.style.width = "100%";
+    pageProduits.style.opacity ="1"
+    boite.style.opacity = "1"
 }
+
