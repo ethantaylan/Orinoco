defce03ac22183077e25d1b2451f4d9d29295329
Ethan TAYLAN <ethtaylan@gmail.com>
2021-05-30 04:19:37 +0200

    js train

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var navProduits = document.getElementById ("nav-produits")
 var navPanier = document.getElementById ("navPanier")
 var navContact = document.getElementById ("nav-contact")
 var close = document.getElementById("close")
-var boite = document.querySelector(".box-teddy")
+var boiteA = document.querySelector("#box-teddy-a")
+var boiteZ = document.querySelector("#box-teddy-z")
+var boiteE = document.querySelector("#box-teddy-e")
+var boiteR = document.querySelector("#box-teddy-r")
+var boiteT = document.querySelector("#box-teddy-t")
 var pageWelcome = document.getElementById("page-welcome")
 var pageAccueil = document.getElementById("page-accueil")
 var pageProduits = document.getElementById("page-produits")
@@ -20,6 +24,10 @@ navEnter.onclick = function() {
 navProduits.onclick = function() {
     pageProduits.style.width = "100%";
     pageProduits.style.opacity ="1"
-    boite.style.opacity = "1"
+    boiteA.style.opacity = "1"
+    boiteZ.style.opacity = "1"
+    boiteE.style.opacity = "1"
+    boiteR.style.opacity = "1"
+    boiteT.style.opacity = "1"
 }
 
