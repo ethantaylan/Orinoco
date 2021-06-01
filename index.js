3a3f3c6796ecf5d2f01e996bb797876af2bfddcf
Ethan TAYLAN <ethtaylan@gmail.com>
2021-06-01 03:54:11 +0200

    -

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ closeImg.addEventListener('click', event => {
 });
 
 document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    if (evt.keyCode == 27) {
+    if (window.event.keyCode == 27) {
         closeImg.style.fontSize = "0";
         tda.style.width ="0"
         tdz.style.width ="0"
@@ -70,3 +69,47 @@ document.onkeydown = function(evt) {
         body.style.opacity ="1"
     }
 };
+
+fetch('http://localhost:3000/api/teddies')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+
+function appendData(data) {
+    console.log(data)
+    var teddy1Price = document.getElementById("td1Price")
+    var teddy2Price = document.getElementById("td2Price")
+    var teddy3Price = document.getElementById("td3Price")
+    var teddy4Price = document.getElementById("td4Price")
+    var teddy5Price = document.getElementById("td5Price")
+    var teddy1Name = document.getElementById("tdName1")
+    var teddy2Name = document.getElementById("tdName2")
+    var teddy3Name = document.getElementById("tdName3")
+    var teddy4Name = document.getElementById("tdName4")
+    var teddy5Name = document.getElementById("tdName5")
+        teddy1Name.innerHTML = data[0].name
+        teddy2Name.innerHTML = data[1].name
+        teddy3Name.innerHTML = data[2].name
+        teddy4Name.innerHTML = data[3].name
+        teddy5Name.innerHTML = data[4].name
+        teddy1Price.innerHTML = data[0].price +"€"
+        teddy2Price.innerHTML = data[1].price +"€"
+        teddy3Price.innerHTML = data[2].price +"€"
+        teddy4Price.innerHTML = data[3].price +"€"
+        teddy5Price.innerHTML = data[4].price +"€"
+    }
+
+const findElement = (array, data) => {
+for (let element of array) {
+if (element === data) {
+return true;
+}
+}
+return false;
+}
+
+console.log(data)
+
